885a9d7b62fb45ce6c443e7e6745a705c3418e1f
xwinkX <opalyuk.e88@ukr.net>
2022-07-04 16:32:20 +0300

    qwqwq

--- src/commandModal.js
+++ src/commandModal.js
@@ -1,4 +1,10 @@
 const openModal = document.querySelector('#openModal');
-console.log(openModal);
 const closeModal = document.querySelector('#closeModal');
 const backdrop = document.querySelector('js-backdrop');
+
+openModal.addEventListener('click', onOpenModal);
+
+function onOpenModal() {
+  document.body.classList.add('show-modal');
+  console.log(onOpenModal);
+}
