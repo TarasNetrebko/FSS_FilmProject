4dcd915573c8cc97987fdd7da0e6c74c915af03e
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2022-07-06 20:56:27 +0300

    fix loader and gallery

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -11,6 +11,9 @@
 import axios from 'axios';
 
 export default function fetchFromBackend(url, callback) {
+  let loader = document.querySelector(".preloader");
+  console.log(loader);
+  loader.classList.remove("hidden");
   axios
     .get(url)
     .then(async function (response) {
@@ -22,6 +25,8 @@ export default function fetchFromBackend(url, callback) {
       console.log(error);
     })
     .then(function () {
-      // always executed
+      // // always executed
+      loader.classList.add("hidden");
+    
     });
 }
--- src/js/loader.js
+++
@@ -1,7 +0,0 @@
-let preloader = document.querySelector(".preloader");
-window.addEventListener('load', () => {
-    preloader.classList.add("hidden");
-    setTimeout(() => {
-        preloader.remove();
-    }, 600);
-})
