7c22522acccae20dbae141d20aa624309fe8f17a
TarasNetrebko <93833612+TarasNetrebko@users.noreply.github.com>
2022-07-06 19:15:27 +0000

    Deploying to gh-pages from @ TarasNetrebko/FSS_FilmProject@f5b7493d864e13ac37d3639d6d4a5e348985c92d 🚀

--- index.3e923890.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".preloader");window.addEventListener("load",(function(){e.classList.add("hidden"),setTimeout((function(){e.remove()}),600)}))}();
-//# sourceMappingURL=index.3e923890.js.map
--- index.74b6a7de.js
+++
@@ -1,2 +0,0 @@
-let e=document.querySelector(".preloader");window.addEventListener("load",(()=>{e.classList.add("hidden"),setTimeout((()=>{e.remove()}),600)}));
-//# sourceMappingURL=index.74b6a7de.js.map
