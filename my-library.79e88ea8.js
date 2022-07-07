7496ec17f5acb4516c17355b80c4d5c06ecfdb43
TarasNetrebko <93833612+TarasNetrebko@users.noreply.github.com>
2022-07-07 13:29:58 +0000

    Deploying to gh-pages from @ TarasNetrebko/FSS_FilmProject@9a1eec0173e016a6aeda19dae361da432b4b5730 🚀

---
+++ my-library.79e88ea8.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("#scroll-top");function e(){window.scrollTo({top:0,behavior:"smooth"})}function s(){window.pageYOffset>300?o.classList.add("scroll-visible"):o.classList.remove("scroll-visible")}window.addEventListener("scroll",s),o.addEventListener("click",e)}();
+//# sourceMappingURL=my-library.79e88ea8.js.map
---
+++ my-library.f08cdd28.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("#scroll-top");function s(){window.scrollTo({top:0,behavior:"smooth"})}function e(){window.pageYOffset>300?o.classList.add("scroll-visible"):o.classList.remove("scroll-visible")}window.addEventListener("scroll",e),o.addEventListener("click",s);
+//# sourceMappingURL=my-library.f08cdd28.js.map
