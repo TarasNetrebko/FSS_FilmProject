9097b483ea6a92571d6127785c12dc9094f1c246
Sergey Havrik <s.gavrik83@gmail.com>
2022-07-12 19:44:10 +0300

    modal team

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -1,17 +1,20 @@
 const openModal = document.querySelector('#openModal');
 const closeModal = document.querySelector('#closeModal');
 const backdrop = document.querySelector('.js-backdrop');
+const modalTeam = document.querySelector(".modal-team");
 
 openModal.addEventListener('click', onOpenModal);
 closeModal.addEventListener('click', onModalClose);
 backdrop.addEventListener('click', onBackdropClick);
 
 function onOpenModal() {
+  modalTeam.style.display = "block";
   window.addEventListener('keydown', onEscPress);
   document.body.classList.add('show-modal');
   document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 }
 function onModalClose() {
+  modalTeam.style.display = "none";
   window.removeEventListener('keydown', onEscPress);
   document.body.classList.remove('show-modal');
   document.getElementsByTagName('body')[0].style.overflow = 'scroll';
